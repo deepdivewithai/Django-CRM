c25d4664d044d4602ec19077abfd83582b18b568
Chetan <chetan.aiofficial@gmail.com>
2024-02-14 22:12:54 +0530

    SignUp and LeadCreateView

--- leads/urls.py
+++ leads/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('', views.LeadsList.as_view(), name='lead-list'),
     path('agent-list/', views.AgentsList.as_view(), name='agent-list'),
     path('<int:pk>/', views.LeadDetailView.as_view(), name='lead-detail'),
+    path('<int:pk>/lead-create', views.LeadsCreateView, name='lead-create'),
+    path('agent-create/', views.AgentCreateView.as_view(), name='agent-create'),
     path('<int:pk>/delete', views.LeadDeleteView.as_view(), name='lead-delete'),
     path('<int:pk>/update', views.LeadUpdateView.as_view(), name='lead-update'),
-    path('lead-create/', views.LeadCreateView.as_view(), name='lead-create'),
-    path('agent-create/', views.AgentCreateView.as_view(), name='agent-create'),
+    path('<int:pk>/detail', views.AgentDetailView.as_view(), name='agent-detail'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .forms import AgentModelForm, LeadModelForm
@@ -10,6 +10,11 @@ class AgentsList(ListView):
     template_name = 'agents/agent_list.html'
     queryset = Agent.objects.all()
     context_object_name = 'agents'
+
+class AgentDetailView(DetailView):
+    template_name = "agents/agent_detail.html"
+    queryset = Agent.objects.all()
+    context_object_name = 'agent'
     
 class AgentCreateView(CreateView):
     template_name = 'agents/agent_create.html'
@@ -32,16 +37,24 @@ class LeadDetailView(DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = 'lead'
-     
-class LeadCreateView(CreateView):
-    template_name = 'leads/lead_create.html'
-    form_class = LeadModelForm
 
-    def get_success_url(self) -> str:
-        return reverse("leads:lead-list")
+def LeadsCreateView(request, pk):
+    if request.method == "POST":
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        age = request.POST['age']
+        email = request.POST['email']
+        agent = Agent.objects.get(id=pk)
+
+        Lead.objects.create(first_name=first_name,
+                            last_name=last_name,
+                            age=age,
+                            email=email,
+                            agent=agent)
+        
+        return redirect("leads:lead-list")
     
-    def form_valid(self, form):
-        return super(LeadCreateView, self).form_valid(form)
+    return render(request, 'leads/lead_create.html')
 
 class LeadUpdateView(UpdateView):
     template_name = 'leads/lead_update.html'
